57dc832d3864fea0dcc2760aeeaf1999ceee37cc
Leandro Alencar <zzleandrobritozz@gmail.com>
2024-02-15 00:08:39 -0300

    feat(route): add first post route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,12 +1,42 @@
 import { Elysia, t } from 'elysia'
+import { db } from '../db/connection'
+import { restaurants, users } from '../db/schema'
 
 const app = new Elysia()
   .get('/', () => 'Hello Elysia')
-  .get('/id/:id', ({ params: { id } }) => id, {
-    params: t.Object({
-      id: t.Numeric(),
-    }),
-  })
+  .post(
+    '/restaurants',
+    async ({ body, set }) => {
+      const { restaurantName, managerName, email, phone } = body
+
+      const [manager] = await db
+        .insert(users)
+        .values({
+          name: managerName,
+          email,
+          phone,
+          role: 'manager',
+        })
+        .returning({
+          id: users.id,
+        })
+
+      await db.insert(restaurants).values({
+        name: restaurantName,
+        managerId: manager.id,
+      })
+
+      set.status = 204
+    },
+    {
+      body: t.Object({
+        restaurantName: t.String(),
+        managerName: t.String(),
+        email: t.String({ format: 'email' }),
+        phone: t.String(),
+      }),
+    },
+  )
 
 app.listen(3000, () => {
   console.log(

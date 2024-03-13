9a22ddd37a54f603f3de893aaf2c07213a766723
Leandro Alencar <zzleandrobritozz@gmail.com>
2024-03-13 17:46:27 -0300

    add error handling

---
+++ src/http/routes/get-managed-restaurant.ts
@@ -0,0 +1,21 @@
+import { Elysia } from 'elysia'
+import { auth } from '../auth'
+import { db } from '../../db/connection'
+
+export const getManagedRestaurant = new Elysia()
+  .use(auth)
+  .get('/managed-restaurant', async ({ getCurrentUser }) => {
+    const { restaurantId } = await getCurrentUser()
+
+    if (!restaurantId) throw new Error('Unauthorized')
+
+    const restaurant = await db.query.restaurants.findFirst({
+      where(fields, { eq }) {
+        return eq(fields.id, restaurantId)
+      },
+    })
+
+    if (!restaurant) throw new Error('Restaurant not found')
+
+    return restaurant
+  })
--- src/http/server.ts
+++ src/http/server.ts
@@ -4,6 +4,7 @@ import { sendAuthLink } from './routes/send-auth-link'
 import { authenticateFromLink } from './routes/authenticate-from-link'
 import { signOut } from './routes/sign-out'
 import { getProfile } from './routes/get-profile'
+import { getManagedRestaurant } from './routes/get-managed-restaurant'
 
 const app = new Elysia()
   .use(registerRestaurant)
@@ -11,6 +12,21 @@ const app = new Elysia()
   .use(authenticateFromLink)
   .use(signOut)
   .use(getProfile)
+  .use(getManagedRestaurant)
+  .onError(({ code, error, set }) => {
+    switch (code) {
+      case 'VALIDATION': {
+        set.status = error.status
+
+        return error.toResponse()
+      }
+      default: {
+        console.error(error)
+
+        return new Response(null, { status: 500 })
+      }
+    }
+  })
 
 app.listen(3000, () => {
   console.log(
